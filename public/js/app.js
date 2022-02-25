777bc9e34a797e9745270fef10dd0a48eecea0bf
Rodolfo Redlich <rodolfo.redlich@zembia.cl>
2022-02-25 00:25:55 -0300

    First commit: Funciontal webapp take a clipping file and extract the highlights

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+function myfunction() { 
+    document.getElementById('bookTitle').innerHTML = gon.clippings[0]["book_title"];
+    document.getElementById('bookAuthor').innerHTML = "myAuthor";
+    console.log(gon.clippings)
+} 
