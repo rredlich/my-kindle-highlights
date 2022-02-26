7434ed3de87b3af92b67e8f9f2659d7d1dab7de1
Rodolfo Redlich <rodolfo.redlich@zembia.cl>
2022-02-26 14:55:49 -0300

    FIX: change index page, sidebar style in result navbar

--- public/js/app.js
+++
@@ -1,5 +0,0 @@
-function myfunction() { 
-    document.getElementById('bookTitle').innerHTML = gon.clippings[0]["book_title"];
-    document.getElementById('bookAuthor').innerHTML = "myAuthor";
-    console.log(gon.clippings)
-} 
